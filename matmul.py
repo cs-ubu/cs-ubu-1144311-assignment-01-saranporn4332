b12a396ceef6629a40b08584882f627ea9688828
saranporn4332 <33215351+saranporn4332@users.noreply.github.com>
2019-07-20 20:44:52 +0700

    Update matmul.py

--- matmul.py
+++ matmul.py
@@ -1,25 +1,27 @@
 import numpy as np
 import csv
 
+'''def csv2mat(filename):
+    return np.array(list(csv.reader(open(filename, "r"), delimiter=",")))
+A=csv2mat('A.csv')
+b=csv2mat('b.csv')
+'''
 
-def readm(fname ='A.csv'):
-    f = open(fname, 'r') # w,b
-    A = []
-    for line in f.readline():
-        A.append( [float(x) for x in line.strip().split(',')] )
-    print(A)
+def readm(fname='A.csv'):
+    f= open(fname,'r')
+    A=[]
+    for line in f.readlines():
+        A.append([float(x) for x in line.strip().split(',')])
     f.close()
-return A
+    return A
 
 def matmul(A,b):
-    m, n = len(A), len(b[0])
-    J = len( A[0] ) # A-mxJ #b -Jxn
-    if len(A[0]) == len(b):
-        #C = [ [0]*n for i in range(n) ]
-        C = [[0]*n]*m
-       # A[0][0]*b[0][0] + A[0][1]*b[1][0] + A[0][2]*b[2][0]
+    m,n=len(A),len(b[0])
+    J = len(A[0])
+    if len(A[0])==len(b):
+        C=[ [0]*n for i in range(m)]
         for r in range(m):
-            for c in range(n): 
-                C[r][c] = sum([float(A[c][j])*float(b[j][c]) for j in range(J)])
+            for c in range(n):
+                C[r][c] = sum([float(A[r][j])*float(b[j][c]) for j in range(J)])
         return C
     return [] #  ไม่สามารถดูได้
